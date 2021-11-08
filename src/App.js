ca57c3d5e4c24adae3d0662133492727e9ad4afc
DeseanOrBusta <72827190+DeseanOrBusta@users.noreply.github.com>
2021-11-08 17:24:19 -0600

    i added an array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
 import "./styles.css";
 
+function Person(name, pronouns, school, year, color, social, animal, nickname) {
+  return (
+    <div class="row">
+      <div class="red">{name}</div>
+      <div>{pronouns}</div>
+      <div class="red">{school}</div>
+      <div>{year}</div>
+      <div class="red">{color}</div>
+      <div>{social}</div>
+      <div class="red">{animal}</div>
+      <div>{nickname}</div>
+    </div>
+  );
+}
 export default function App() {
+  const lily = Person(
+    "Lilly Minchala",
+    "She/Hers",
+    "HHSLT",
+    2022,
+    "purple",
+    "Insta",
+    "Rats",
+    "Lillian"
+  );
+  const claudia = Person(
+    "Luna-Marcelino",
+    "She/Hers",
+    "P-TECH",
+    2022,
+    "",
+    "",
+    "Cats",
+    "Luna"
+  );
+  const Desean = Person(
+    "Desean-Blackburn",
+    "He/Him",
+    "Allen",
+    2022,
+    "Green",
+    "Insta",
+    "Dogs",
+    "Busta"
+  );
   return (
     <div className="App">
-      <ol>
-        <li></li>
-        </ol>
+      {lily}
+      {claudia}
+      {Desean}
     </div>
   );
 }
 
-function createrosteritem(studentName, StudentPronouns, studenSchool){
-rosterItem - "<li>(studentName) + '       ' + StudentProunouns + '       '+ StudentSchool </li>"
+function displayPerson(first,last)
 
+export default function app() {
+  let people= [
+    ["lily","Minchala"]
+    ["Luna","Marcelino"]
+    ["Desean","Blackburn"]
+  ];
+
+  let elements = [];
+  for (let person of people){
+    let 
+  }
 }
